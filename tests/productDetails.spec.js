3d5ff3b5a0e683e1b5a7fd5991321e84fa5ee994
Carlos Barros <carlos_dbs@hotmail.com>
2021-07-27 18:43:56 -0300

    Finalizacao dos teste productDetails

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -38,7 +38,11 @@ describe('6 - Implemente os casos de teste para a função `productDetails`', ()
     // Teste que o array retornado pela função contém dois itens dentro.
     assert.strictEqual(product.length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.strictEqual(typeof product[0] && typeof product[1], 'object');
     // Teste que os dois objetos são diferentes entre si.
+    assert.notDeepStrictEqual(product[0], product[1]);
     // Teste que os dois productIds terminam com 123.
+    /** source:https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/endsWith */
+    assert.strictEqual(product[0].details.productId.endsWith('123') && product[1].details.productId.endsWith('123'), true);
   });
 });
