a27967742199384e8a8305bae977d9efa4b7637f
Carlos Barros <carlos_dbs@hotmail.com>
2021-07-27 16:40:20 -0300

    Finalizacao de testes para circle

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -34,7 +34,12 @@ describe('4 - Implemente os casos de teste para a função `circle`', () => {
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
     assert.strictEqual(circle(), undefined);
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
+    const circleObject_2 = circle(2);
+    assert.strictEqual(circleObject_2.circumference, 12.56)
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
+    const circleObject_3 = circle(3);
+    assert.strictEqual(parseFloat(circleObject_3.area.toPrecision(4)), 28.26)
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
+    assert.deepStrictEqual(circle(3), circleObject_3);
   });
 });
