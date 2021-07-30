d220aca87877daabe6dbcc646047cec21c65e56f
Carlos Barros <carlos_dbs@hotmail.com>
2021-07-30 10:40:19 -0300

    Refatoracao de getCharacter.spec.js e productDetails.spec.js

--- tests/getCharacter.spec.js
+++ tests/getCharacter.spec.js
@@ -1,8 +1,8 @@
 /* eslint-disable max-len */
 /* eslint-disable no-unused-vars */
 
-const assert = require('assert');
-const getCharacter = require('../src/getCharacter');
+const assert = require("assert");
+const getCharacter = require("../src/getCharacter");
 
 /*
 Essa função recebe como parâmetro o nome de um personagem e retorna um objeto com seu nome, classe e frases.
@@ -44,20 +44,48 @@ Retorno:
 OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
 */
 
-describe('9 - Implemente os casos de teste da função `getCharacter`', () => {
-  it('Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.', () => {
+describe("9 - Implemente os casos de teste da função `getCharacter`", () => {
+  it("Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.", () => {
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
     assert.strictEqual(getCharacter(), undefined);
     // Teste se a função retorna o objeto correto para o parâmetro 'Arya',
-    assert.deepStrictEqual(getCharacter('Arya'), {name: 'Arya Stark', class: 'Rogue' , phrases: ['Not today', 'A girl has no name.']})
+    assert.deepStrictEqual(getCharacter("Arya"), {
+      name: "Arya Stark",
+      class: "Rogue",
+      phrases: ["Not today", "A girl has no name."],
+    });
     // Teste se a função retorna o objeto correto para o parâmetro 'Brienne',
-    assert.deepStrictEqual(getCharacter('Brienne'), {name: 'Brienne Tarth', class: 'Knight' , phrases: ['Im No Lady, Your Grace.', 'I, Brienne Of Tarth, Sentence You To Die.']})
+    assert.deepStrictEqual(getCharacter("Brienne"), {
+      name: "Brienne Tarth",
+      class: "Knight",
+      phrases: [
+        "Im No Lady, Your Grace.",
+        "I, Brienne Of Tarth, Sentence You To Die.",
+      ],
+    });
     // Teste se a função retorna o objeto correto para o parâmetro 'Melissandre',
-    assert.deepStrictEqual(getCharacter('Melissandre'), {name: 'Melissandre', class: 'Necromancer' , phrases: ['Death By Fire Is The Purest Death.', 'For The Night Is Dark And Full Of Terrors.']})
+    assert.deepStrictEqual(getCharacter("Melissandre"), {
+      name: "Melissandre",
+      class: "Necromancer",
+      phrases: [
+        "Death By Fire Is The Purest Death.",
+        "For The Night Is Dark And Full Of Terrors.",
+      ],
+    });
     // Teste se a função se os parâmetros não são Case Sensitive.
-    assert.deepStrictEqual(getCharacter('ArYa') || getCharacter('ARYA'), {name: 'Arya Stark', class: 'Rogue' , phrases: ['Not today', 'A girl has no name.']})
+    assert.deepStrictEqual(getCharacter("ArYa"), {
+      name: "Arya Stark",
+      class: "Rogue",
+      phrases: ["Not today", "A girl has no name."],
+    });
+
+    assert.deepStrictEqual(getCharacter("ARYA"), {
+      name: "Arya Stark",
+      class: "Rogue",
+      phrases: ["Not today", "A girl has no name."],
+    });
     // Teste se ao passar um nome que não está na tabela, a função retorna undefined.
-    assert.strictEqual(getCharacter('Bruce'), undefined);
+    assert.strictEqual(getCharacter("Bruce"), undefined);
   });
 });
--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -1,6 +1,6 @@
-const assert = require('assert');
-const { type } = require('os');
-const productDetails = require('../src/productDetails');
+const assert = require("assert");
+const { type } = require("os");
+const productDetails = require("../src/productDetails");
 
 /*
   Dadas duas strings que representam nomes de produtos, retorne um array contendo dois objetos com os detalhes dos respectivos produtos.
@@ -29,20 +29,25 @@ const productDetails = require('../src/productDetails');
   OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
 */
 
-describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
-  it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
+describe("6 - Implemente os casos de teste para a função `productDetails`", () => {
+  it("Verifica se a função `productDetails` tem o comportamento esperado", () => {
     // ESCREVA SEUS TESTES ABAIXO:
-    const product = productDetails('Alcool gel', 'Máscara');
+    const product = productDetails("Alcool gel", "Máscara");
     // Teste que o retorno da função é um array.
-    assert.strictEqual(typeof product, 'object');
+    assert.strictEqual(typeof product, "object");
     // Teste que o array retornado pela função contém dois itens dentro.
     assert.strictEqual(product.length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
-    assert.strictEqual(typeof product[0] && typeof product[1], 'object');
+    assert.strictEqual(typeof product[0], "object");
+    assert.strictEqual(typeof product[1], "object");
     // Teste que os dois objetos são diferentes entre si.
     assert.notDeepStrictEqual(product[0], product[1]);
     // Teste que os dois productIds terminam com 123.
     /** source:https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/endsWith */
-    assert.strictEqual(product[0].details.productId.endsWith('123') && product[1].details.productId.endsWith('123'), true);
+    assert.strictEqual(
+      product[0].details.productId.endsWith("123") &&
+        product[1].details.productId.endsWith("123"),
+      true
+    );
   });
 });
